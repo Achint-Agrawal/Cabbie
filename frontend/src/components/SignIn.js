731f3cf3eeafd2e7a502f5a76919a3be9b6d0911
Achint <ag.achint@gmail.com>
2022-04-20 16:22:33 +0530

    resolved merge conflicts

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -82,19 +82,21 @@ export default function SignIn({ setToken, setUserType }) {
                 navigate("/");
             })
             .catch((err) => {
-        // console.log("err", err.response.status);
+                // console.log("err", err.response.status);
 
-        const status = err.response.status;
+                const status = err.response.status;
 
-        if(status === 404){
-          alert("Email not registered!!");
-        }
-        else if(status === 400){
-          alert("Incorrect Password!!");
-        }
-        setStay(true);
-        errorAlert = <p style={{ color: "red" }}>some error occurred, try again!!</p>
-      })
+                if (status === 404) {
+                    alert("Email not registered!!");
+                } else if (status === 400) {
+                    alert("Incorrect Password!!");
+                }
+                setStay(true);
+                errorAlert = (
+                    <p style={{ color: "red" }}>
+                        some error occurred, try again!!
+                    </p>
+                );
             });
     };
 
